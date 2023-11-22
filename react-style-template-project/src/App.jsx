47bef7b438f926f6844777d126cb3783e915d70e
YaqubAliyev4513 <tu6w6s3r7@code.edu.az>
2023-11-22 04:17:41 +0400

    clean code(hero) = time economing

--- react-style-template-project/src/App.jsx
+++ react-style-template-project/src/App.jsx
@@ -13,6 +13,8 @@ function App() {
       <Hero heroTitle={"Stylish Portfolio"} heroText={"A Free Bootstrap Theme by Start Bootstrap"} heroButtonText={"Find out more"} />
       <SubHero subHeroTitle={"Stylish Portfolio is the perfect theme for your next project!"} subHeroText={"This theme features a flexible, UX friendly sidebar menu and stock photos from our friends at Unsplash !"}  subHeroButtonText="What we offer" />
       <BlueLayout/>
+      <Hero heroMediumTitle={"Welcome to your next web site"} heroButtonText={"Download now"}/>
+
     </>
   )
 }
--- react-style-template-project/src/components/Hero.jsx
+++ react-style-template-project/src/components/Hero.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import '../styles/hero.css'
-const Hero = ({heroTitle,heroText,heroButtonText}) => {
+const Hero = ({heroTitle,heroMediumTitle,heroText,heroButtonText}) => {
     return (
         <div className='hero'>
 
             <div className="hero.hero-content">
                 <div className="hero-title">
                     {heroTitle}
+                </div>
+                <div className="hero-medium-title">
+                    {heroMediumTitle}
                 </div>
                 <div className="hero-text">
                     {heroText}
